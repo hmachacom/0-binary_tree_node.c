1ac33575bac42b891c1d0eec98e371b6ad7067a9
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-03-22 13:16:20 -0500

    four point

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - Verificamos si es o no una hoja
+ * @node: raiz
+ * Return: 0 si falla, 1 si sirve
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (!(node->right) && !(node->left) && node->parent)
+	{
+		return (1);
+	}
+	return (0);
+}
