5243ecb35702d2e13444f7820285b6f730d70765
Hugo Machacon <hmachacom@gmail.com>
2022-03-23 06:29:38 -0500

    aun no esta listo

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,5 +6,17 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	
+	if (!tree)
+		return (0);
+	if (tree->right && tree->left)
+		return (binary_tree_balance(tree->left) + (0) + binary_tree_balance(tree->right));
+	else
+	{
+		if (tree->right || tree->left)
+			return (binary_tree_balance(tree->left) + (-1) + binary_tree_balance(tree->right));
+		else
+		{
+		return (0);
+		}
+	}
 }
