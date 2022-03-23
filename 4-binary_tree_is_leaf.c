87f8027ae7708e9c50ab2fbc1cad87e43bf5cb5c
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-03-23 17:51:51 -0500

    correcion 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,11 +6,9 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node != NULL)
+	if (node == NULL)
 		return (0);
 	if (!(node->right) && !(node->left) && node->parent)
-	{
 		return (1);
-	}
 	return (0);
 }
