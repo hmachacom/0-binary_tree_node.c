d597087eab37b1ee36f33c4ed30a62928fc7670f
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-03-23 18:33:28 -0500

    catorce

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,35 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance -
- * @tree:
- * Return:
-*/
+ * binary_tree_balance -Mide el factor de equilibrio de un árbol binario.
+ * @tree: raiz
+ * Return: el resultado de la operacion.
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	if (tree->right && tree->left)
-		return (binary_tree_balance(tree->left) + (0) + binary_tree_balance(tree->right));
-	else
+	if (tree)
+		return (adicional(tree->left) - adicional(tree->right));
+	return (0);
+}
+/**
+ * adicional - Mide la altura de un árbol binario.
+ * @tree: raiz.
+ * Return: valor de la altura del arbol.
+ */
+size_t adicional(const binary_tree_t *tree)
+{
+	if (tree != NULL)
 	{
-		if (tree->right || tree->left)
-			return (binary_tree_balance(tree->left) + (-1) + binary_tree_balance(tree->right));
+		size_t lef = 0, rig = 0;
+
+		if (tree->left)
+			lef = adicional(tree->left) + 1;
 		else
-		{
-		return (0);
-		}
+			lef = 1;
+		if (tree->right)
+			rig = adicional(tree->right) + 1;
+		else
+			rig = 1;
+		return ((lef > rig) ? lef : rig);
 	}
+	return (0);
 }
