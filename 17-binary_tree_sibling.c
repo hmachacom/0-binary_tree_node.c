dc0d581c0f8f012ba0f30e1542075346dc7968df
Hugo Machacon <hmachacom@gmail.com>
2022-03-24 11:19:39 -0500

    17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - ::::::::::::
+ * @node: raiz
+ * Return: pointer or null
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+		if (node->parent->right == node)
+			return (node->parent->left);
+	}
+	return (NULL);
+}
