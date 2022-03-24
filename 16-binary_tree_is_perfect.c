52c2e4e87f728652dc0811a84dc4dbf975723f11
Hugo Machacon <hmachacom@gmail.com>
2022-03-24 13:37:13 -0500

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,75 +1,48 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_perfect - ::::::::::::
- * @tree: raiz
- * Return: 0 or 1
+ * Depth - Function that measures the depth of a node in
+ * a binary tree
+ * @tree: Is a pointer to the node to measure the depth.
+ * Return: Level of tree.
+ */
+size_t Depth(const binary_tree_t *tree)
+{
+	size_t c;
+
+	if (!tree)
+		return (0);
+	for (c = 0; tree; c++)
+		tree = tree->left;
+	return (c);
+}
+/**
+ * is_perfect - Function that checks if a binary tree is perfect.
+ * @tree: Parent node.
+ * @d: tree depth.
+ * @level: Current level.
+ * Return: 1 or 0.
+ */
+int is_perfect(const binary_tree_t *tree, int d, int level)
+{
+	if (!tree->left && !tree->right)
+		return (d == level + 1);
+	if (!tree->left || !tree->right)
+		return (0);
+	return (is_perfect(tree->left, d, level + 1) &&
+			is_perfect(tree->right, d, level + 1));
+}
+/**
+ * binary_tree_is_perfect - Function that say if a binary tree is perfect.
+ * @tree: Is a pointer to the root node or any other node.
+ * Return: 1 if it´s perfect otherwise 0.
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	return ((binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
-			? 1 : 0);
-}
-/**
- * binary_tree_balance - :::::::::::::
- * @tree: raiz
- * Return: 0 or 1
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree)
-	{
-		return (adicional(tree->left) - adicional(tree->right));
-	}
-	return (0);
-}
-/**
- * adicional - Mide la altura de un árbol binario.
- * @tree: raiz.
- * Return: valor de la altura del arbol.
- */
-size_t adicional(const binary_tree_t *tree)
-{
-	if (tree != NULL)
-	{
-		size_t lef = 0, rig = 0;
-
-		if (tree->left)
-			lef = adicional(tree->left) + 1;
-		else
-			lef = 1;
-		if (tree->right)
-			rig = adicional(tree->right) + 1;
-		else
-			rig = 1;
-		return ((lef > rig) ? lef : rig);
-	}
-	return (0);
-}
-/**
- * binary_tree_is_full - comprueba si un árbol binario está lleno
- * @tree: arbol
- * Return: si esta lleno 1 si no 0
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	int lef = 0;
-	int rig = 0;
+	int leng;
 
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
-
-	lef = binary_tree_is_full(tree->left);
-	rig = binary_tree_is_full(tree->right);
-
-	if (lef && rig)
-		return (1);
-
-	return (0);
+	leng = Depth(tree);
+	return (is_perfect(tree, leng, 0));
 }
