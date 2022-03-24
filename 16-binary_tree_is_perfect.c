f0f7c85a318c8f569e3e049cf7d20db0d20abc1d
Hugo Machacon <hmachacom@gmail.com>
2022-03-24 13:34:40 -0500

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,6 +8,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 	return ((binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
 			? 1 : 0);
 }
@@ -19,7 +21,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
+	{
 		return (adicional(tree->left) - adicional(tree->right));
+	}
 	return (0);
 }
 /**
